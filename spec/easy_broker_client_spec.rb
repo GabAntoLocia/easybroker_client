5ebf8e3b065d8669c5922a5f42b0ff238f3dc933
Gabriel Locia <locciaa1@gmail.com>
2024-10-10 17:38:31 -0600

    Add additional test cases for EasyBrokerClient

--- spec/easy_broker_client_spec.rb
+++ spec/easy_broker_client_spec.rb
@@ -15,4 +15,21 @@ RSpec.describe EasyBrokerClient do
 
     expect { client.print_property_titles }.to output("Beautiful Apartment\nSpacious House\n").to_stdout
   end
+
+  it 'raises an error if the API key is missing' do
+    allow(ENV).to receive(:[]).with('EASYBROKER_API_KEY').and_return(nil)
+    expect { EasyBrokerClient.new }.to raise_error(RuntimeError, "API Key not found. Please set the EASYBROKER_API_KEY environment variable.")
+  end
+
+  it 'handles API errors gracefully' do
+    allow(RestClient).to receive(:get).and_raise(RestClient::InternalServerError)
+    expect { client.list_properties }.to raise_error(RestClient::InternalServerError)
+  end
+
+  it 'handles a large number of properties efficiently' do
+    properties = Array.new(1000) { { 'title' => 'Property Title' } }
+    allow(client).to receive(:list_properties).and_return(properties)
+
+    expect { client.print_property_titles }.to output(/Property Title\n/).exactly(1000).times
+  end
 end
